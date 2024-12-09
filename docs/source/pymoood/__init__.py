cf601cc0e8d125f1cca1189dfce8f6b15804c01e
JOGUNHEE14 <arijo2123@hanyang.ac.kr>
2024-12-09 22:49:59 +0900

    add fuction

--- docs/source/pymoood/__init__.py
+++ docs/source/pymoood/__init__.py
@@ -2,3 +2,4 @@ from .Emotion_Analysis_English import analysis_emotion
 from .chatbot import ChatbotKR
 from .playlist import EmotionPredictor, YouTubeRecommender
 from .sentence_emotion import EmotionPredict
+from .badword import filter_profanity
