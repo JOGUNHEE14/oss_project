f781e428a086d6dec6e75fa1c35c3dd4da2fe5bc
JOGUNHEE <123735072+JOGUNHEE14@users.noreply.github.com>
2024-12-09 19:44:14 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
+html_theme = 'sphinx_rtd_theme'
 templates_path = ['_templates']
 exclude_patterns = []
 
